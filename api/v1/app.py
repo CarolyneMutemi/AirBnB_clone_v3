e709f087375a309e080d768977509261efb40a56
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-01-08 19:37:29 +0300

    More API

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 The main API file.
 """
 
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 import os
@@ -18,16 +18,16 @@ def refresh(exception):
     """
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    """
+    Error page.
+    """
+    return jsonify({"error": "Not found"})
+
+
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST')
-    port = os.getenv('HBNB_API_PORT')
-    if host:
-        host_value = host
-    else:
-        host_value = '0.0.0.0'
-    if port:
-        port_value = port
-    else:
-        port_value = '5000'
-    app.run(host=host_value, port=port_value, threaded=True)
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, debug=True, threaded=True)
---
+++ api/v1/views/states.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""
+Handles all default RESTFul API actions for State objects.
+"""
+
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from models.state import State
+from models import storage
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_all_states():
+    """
+    Retrieves the list of all State objects
+    """
+    list_states = []
+    for state in storage.all(State).values():
+        list_states.append(state.to_dict())
+    return jsonify(list_states)
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """
+    Retrieves a State object.
+    """
+    if not storage.get(State, state_id):
+        abort(404)
+    return jsonify(storage.get(State, state_id).to_dict())
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+    """
+    Deletes a State object.
+    """
+    obj = storage.get(State, state_id)
+    if not obj:
+        abort(404)
+    obj.delete()
+    return jsonify({})
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post_state():
+    """
+    Creates a State.
+    """
+    if not request.is_json:
+        return "Not a JSON", 400
+    data = request.get_json()
+    if "name" not in data:
+        return "Missing name", 400
+    new_obj = State(name=data.get("name"))
+    new_obj.save()
+    return jsonify(new_obj.to_dict()), 201
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """
+    Updates a State object.
+    """
+    obj = storage.get(State, state_id)
+    if not obj:
+        abort(404)
+    if not request.is_json:
+        return "Not a JSON", 400
+    data = request.get_json()
+    for key, value in data.items():
+        if key not in ["id", "created_at", "updated_at"]:
+            setattr(obj, key, value)
+    obj.save()
+    return jsonify(obj.to_dict()), 200
