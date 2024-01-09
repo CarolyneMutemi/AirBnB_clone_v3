b00eaf716c68ac3cee7018800294ff1dc3a19b14
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-01-10 02:22:26 +0300

    module app

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,11 +3,11 @@
 The main API file.
 """
 
+import os
 from flask_cors import CORS
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
